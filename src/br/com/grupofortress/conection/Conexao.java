a386f4902f30744c5079fdd5dd5ca85914703840
Lukas Roberto <Lukas.roberto@hormail.com>
2015-06-10 21:34:31 -0300

    teste ignoradno

--- src/br/com/grupofortress/conection/Conexao.java
+++ src/br/com/grupofortress/conection/Conexao.java
@@ -10,7 +10,7 @@ public class Conexao {
 //    private static final String servidor = Propriedades.getProp().getProperty("servidor");
 //    private static final String URL = "jdbc:sqlserver://"+servidor+":1433; databaseName=monitoramento;";//se não for acessar localmente mude localhost pelo nome do servidor  
 //    private static final String usuario = "sa";//esse usuário é um sysadmin ele tem todos os poderes, é bom que se crie um login e usuário a parte  
-//    private static final String password = "spotGF2008";
+//    private static final String password = "senha";
 //    private static final String DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver"; //Esse é o nome do driver, que na internet você vai encontrar de varias maneiras, mas só esse resolveu meus problemas  
 //
 //    public static Connection abreConexao() throws SQLException {
